95688ed0e8931f9cf61def140b8e124a05d6d7ac
JOATLGTM <66840886+JOATLGTM@users.noreply.github.com>
2024-02-20 12:57:41 -0600

    chore: update project description

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -58,7 +58,7 @@ export const Projects = () => {
               {({ isVisible }) =>
                 <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                   <h2>Projects</h2>
-                  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                  <p>Here are my projects showcasing my proficiency in HTML5, CSS3, and JavaScript for a responsive front-end using React.js, Remix.js and Redux. The robust back-end, built on Node.js and Express.js, integrates MongoDB for scalable database management.</p>
                   <Tab.Container id="projects-tabs" defaultActiveKey="first">
                     <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
                       <Nav.Item>
